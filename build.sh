88f672291fb759c9164a061a976b68445efa56c7
Jelle Z <jellezwiers@icloud.com>
2020-02-27 14:33:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#  __  __ _    ____
-# |  \/  | | _|  _ \  ___   ___ ___
-# | |\/| | |/ / | | |/ _ \ / __/ __|
-# | |  | |   <| |_| | (_) | (__\__ \
-# |_|  |_|_|\_\____/ \___/ \___|___/
-
-mkdocs build --clean --config-file mkdocs.yml --site-dir wiki
-
 #  ______     ___   _ 
 # / ___\ \   / / \ | |
 # \___ \\ \ / /|  \| |
@@ -15,3 +7,11 @@ mkdocs build --clean --config-file mkdocs.yml --site-dir wiki
 # |____/  \_/  |_| \_|
  
 python3 svn export https://github.com/jelle619/jellezwie.rs/trunk/mkdocs/characters/docs/Dragon%20RP docs/Characters
+
+#  __  __ _    ____
+# |  \/  | | _|  _ \  ___   ___ ___
+# | |\/| | |/ / | | |/ _ \ / __/ __|
+# | |  | |   <| |_| | (_) | (__\__ \
+# |_|  |_|_|\_\____/ \___/ \___|___/
+
+mkdocs build --clean --config-file mkdocs.yml --site-dir wiki
